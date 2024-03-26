fa9011f44ccefd3dd666f65e035e938df67fb9b1
Chunxing Shen <chunxing.shen@gmail.com>
2024-03-26 00:00:26 -0500

    #2 Initial set up for Reqnroll

--- test/Phoenix.Core.ReqnrollNUnitTest/StepDefinitions/BirdSpeciesStepDefinitions.cs
+++ test/Phoenix.Core.ReqnrollNUnitTest/StepDefinitions/BirdSpeciesStepDefinitions.cs
@@ -1,11 +1,11 @@
 using NUnit.Framework;
 
-namespace Phoenix.Core.SpecFlowTest.StepDefinitions
+namespace Phoenix.Core.ReqnrollNUnitTest.StepDefinitions
 {
     [Binding]
     public sealed class BirdSpeciesStepDefinitions
     {
-        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        // For additional details on Reqnroll step definitions see https://go.reqnroll.net/doc-stepdef
 
         BirdSpecies _birdSpecies;
 
@@ -14,6 +14,7 @@ namespace Phoenix.Core.SpecFlowTest.StepDefinitions
         public void GivenIEnterABirdSpeciesWithTheFollowingValues(Table table)
         {
             _birdSpecies = EnterABirdSpeciesFromTable(table);
+
         }
 
         BirdSpecies EnterABirdSpeciesFromTable(Table table)
@@ -43,5 +44,40 @@ namespace Phoenix.Core.SpecFlowTest.StepDefinitions
             Assert.AreEqual(expectedScientificName, _birdSpecies.ScientificName);
         }
 
+        //[Given("the first number is {int}")]
+        //public void GivenTheFirstNumberIs(int number)
+        //{
+        //    //TODO: implement arrange (precondition) logic
+        //    // For storing and retrieving scenario-specific data see https://go.reqnroll.net/doc-sharingdata
+        //    // To use the multiline text or the table argument of the scenario,
+        //    // additional string/Table parameters can be defined on the step definition
+        //    // method. 
+
+        //    throw new PendingStepException();
+        //}
+
+        //[Given("the second number is {int}")]
+        //public void GivenTheSecondNumberIs(int number)
+        //{
+        //    //TODO: implement arrange (precondition) logic
+
+        //    throw new PendingStepException();
+        //}
+
+        //[When("the two numbers are added")]
+        //public void WhenTheTwoNumbersAreAdded()
+        //{
+        //    //TODO: implement act (action) logic
+
+        //    throw new PendingStepException();
+        //}
+
+        //[Then("the result should be {int}")]
+        //public void ThenTheResultShouldBe(int result)
+        //{
+        //    //TODO: implement assert (verification) logic
+
+        //    throw new PendingStepException();
+        //}
     }
 }
--- test/Phoenix.Core.SpecFlowNUnitTest/Features/BirdSpecies.feature.cs
+++ test/Phoenix.Core.SpecFlowNUnitTest/Features/BirdSpecies.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Phoenix.Core.SpecFlowTest.Features
+namespace Phoenix.Core.SpecFlowNUnitTest.Features
 {
     using TechTalk.SpecFlow;
     using System;
